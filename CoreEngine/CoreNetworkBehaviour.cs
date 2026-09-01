82515bd3cd75e5f09e12096f96cb9c29aded5c5d
YoonChangJoon <ycjun1209@gmail.com>
2026-09-02 05:00:47 +0900

    fix: FishNet Weaver로 인한 잠재적인 오류 사전차단

--- CoreEngine/CoreNetworkBehaviour.cs
+++ CoreEngine/CoreNetworkBehaviour.cs
@@ -24,8 +24,22 @@ namespace CoreEngine.Network.FishNetExtension
         // ref로 넘기기 위해 인스턴스가 쥐고 있는 상태값
         private bool _isRegistered = false;
 
+        // 위버가 코드를 안전하게 찔러넣을 수 있는 공간이면서 외부 접근은 완벽히 차단
+        protected virtual void Awake()
+        {
+            // 컴파일 시 FishNet Weaver가 여기에 NetworkInitialize___Early() 등을 몰래 주입할 수 있음
+        }
+
+        protected virtual void OnDestroy()
+        {
+            // 컴파일 시 FishNet Weaver가 여기에 네트워크 메모리 해제 로직을 몰래 주입할 수 있음
+        }
+
+
         protected virtual void OnEnable()
         {
+            // 잠재적 Null 오류 원천 차단: 네트워크 뼈대가 조립되기 전이라면 무시
+            if (base.NetworkObject == null) return;
             if (base.IsSpawned) this.TryRegisterNetworkTick(ref _isRegistered, networkTickTarget);
         }
 
