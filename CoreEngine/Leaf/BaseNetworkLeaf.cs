20583d6e77a2385477555b7e16df15caea26ddb8
YoonChangJoon <ycjun1209@gmail.com>
2026-09-02 10:24:07 +0900

    BaseNetworkLeaf 중복코드 정리, Scene 확장기능에 맞춰서 FishNet Extension Scene 준비

--- CoreEngine/Leaf/BaseNetworkLeaf.cs
+++ CoreEngine/Leaf/BaseNetworkLeaf.cs
@@ -1,3 +1,4 @@
+using CoreEngine.Extentions;
 using UnityEngine;
 
 namespace CoreEngine.Network.FishNetExtension
@@ -26,27 +27,7 @@ namespace CoreEngine.Network.FishNetExtension
         protected override void OnValidate()
         {
             base.OnValidate();
-
-            // 아직 스코프가 None(미지정) 상태일 때만 자동 추론을 작동시킵니다.
-            if (myScope == ContextScope.None)
-            {
-                // 현재 이 스크립트가 물리적으로 배치된 씬의 이름을 확인합니다.
-                // 하이라키에 올려두는 순간 즉시 판별됩니다.
-                string mySceneName = gameObject.scene.name;
-
-                if (mySceneName == ProjectContext.GlobalScene)
-                {
-                    myScope = ContextScope.Project;
-                    // 에디터 인스펙터 창의 값을 강제로 갱신하고 저장 상태로 만듭니다.
-                    UnityEditor.EditorUtility.SetDirty(this);
-                }
-                else if (!string.IsNullOrEmpty(mySceneName))
-                {
-                    // 글로벌 씬이 아닌 일반 씬에 배치되었다면 안전하게 Scene 소속으로 고정해 줍니다.
-                    myScope = ContextScope.Scene;
-                    UnityEditor.EditorUtility.SetDirty(this);
-                }
-            }
+            this.AutoSetupScope(ref myScope);
         }
 #endif
     }
