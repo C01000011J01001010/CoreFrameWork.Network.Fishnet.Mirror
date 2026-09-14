0287a9c362f29238d069999e55591b82d5e49d8b
YoonChangJoon <ycjun1209@gmail.com>
2026-09-14 10:17:53 +0900

    refactor: BaseNetworkActor의 safeSpawn로직을 상위 객체인 CoreNetworkBehaviour로 이동, CoreNetworkBehaviour에서 ISpawnable을 구현 후 OnSafeSpawn, OnSafeDespawn을 제공하도록 함

--- CoreEngine/CoreNetworkBehaviour.cs
+++ CoreEngine/CoreNetworkBehaviour.cs
@@ -7,6 +7,7 @@ using FishNet;
 using FishNet.Connection;
 using FishNet.Object;
 using UnityEngine;
+using System.Collections;
 
 namespace CoreEngine.Network.FishNetExtension
 {
@@ -22,7 +23,7 @@ namespace CoreEngine.Network.FishNetExtension
     /// <summary>
     /// FishNet 네트워크 생명주기와 Tick 자동 등록/해제를 통제하는 뼈대 클래스
     /// </summary>
-    public abstract class CoreNetworkBehaviour : NetworkBehaviour
+    public abstract class CoreNetworkBehaviour : NetworkBehaviour, ISpawnable
     {
         protected abstract NetworkTickTarget networkTickTarget { get; }
 
@@ -33,10 +34,14 @@ namespace CoreEngine.Network.FishNetExtension
         protected bool IsProjectReady => CoreFacadeState.ProjectInit;
         protected bool IsSceneReady => CoreFacadeState.SceneInit;
 
+        protected IEnumerator _deferredSpawnRoutine;
+        protected IEnumerator DeferredspawnRoutine => _deferredSpawnRoutine;
+
         // 위버가 코드를 안전하게 찔러넣을 수 있는 공간
         // 컴파일 시 FishNet Weaver가 여기에 NetworkInitialize___Early() 등을 몰래 주입할 수 있음
         public virtual void Awake() { }
 
+        #region Update
         protected virtual void OnEnable()
         {
             // 잠재적 Null 오류 원천 차단: 네트워크 뼈대가 조립되기 전이라면 무시
@@ -58,15 +63,79 @@ namespace CoreEngine.Network.FishNetExtension
             this.TryUnregisterNetworkTick(ref _isRegistered);
             this.TryRegisterNetworkTick(ref _isRegistered, networkTickTarget);
         }
+        #endregion
 
-        public override void OnStartNetwork()
+        #region Spawn
+
+        /// <summary>
+        /// 네트워크 Spawn 이벤트의 진입점
+        /// Scene 초기화가 완료된 후 실제 Spawn 로직을 실행
+        /// </summary>
+        public void OnSpawn()
         {
-            // Spawn된 객체
-            if(this is IPoolable)
+            if (_deferredSpawnRoutine != null)
+                return;
+
+            _deferredSpawnRoutine = DeferredSpawnRoutine();
+            StartCoroutine(_deferredSpawnRoutine);
+        }
+
+        private IEnumerator DeferredSpawnRoutine()
+        {
+            // Scene 시스템이 준비될 때까지 대기
+            while (!IsSceneReady)
+                yield return null;
+
+            // 대기 도중 Despawn되어 Pool로 반환되었거나
+            // NetworkObject가 더 이상 Spawn 상태가 아니라면 종료
+            if (this == null ||
+                !gameObject.activeInHierarchy ||
+                !NetworkObject.IsSpawned)
             {
-                var scene = NetworkObject.IsGlobal ? CoreFacade.GetGlobalScene() : CoreFacade.GetCurrentScene();
+                _deferredSpawnRoutine = null;
+                yield break;
+            }
+
+            // Root 객체라면 Object 설정에 따라 Scene 이동
+            if (transform.parent == null)
+            {
+                var scene = NetworkObject.IsGlobal
+                    ? CoreFacade.GetGlobalScene()
+                    : CoreFacade.GetCurrentScene();
+
                 gameObject.MoveScene(scene);
             }
+
+            // Scene 인프라와 객체 상태가 보장된 시점에서 실제 Spawn 로직 실행
+            OnSafeSpawn();
+
+            _deferredSpawnRoutine = null;
         }
+
+        /// <summary>
+        /// Scene 인프라가 준비되고 객체가 유효한 상태에서 실행되는 실제 Spawn 로직
+        /// 자식 클래스는 이 메서드를 재정의하여 Spawn 시 필요한 초기화를 수행
+        /// </summary>
+        protected virtual void OnSafeSpawn() { }
+
+        public void OnDespawn()
+        {
+            // Spawn 대기 중이었다면 Coroutine을 종료
+            if (_deferredSpawnRoutine != null)
+            {
+                StopCoroutine(_deferredSpawnRoutine);
+                _deferredSpawnRoutine = null;
+            }
+
+            OnSafeDespawn();
+        }
+
+        /// <summary>
+        /// Despawn 시 실행되는 실제 정리 로직
+        /// </summary>
+        protected virtual void OnSafeDespawn() { }
+
+
+        #endregion
     }
 }
--- CoreEngine/Leaf/BaseNetworkActor.cs
+++ CoreEngine/Leaf/BaseNetworkActor.cs
@@ -27,42 +27,6 @@ namespace CoreEngine.Network.FishNetExtension
             EventBus<ActorRegistrationEvent>.Publish(evt);
         }
 
-        protected IEnumerator _deferredSpawnRoutine;
-        protected IEnumerator DeferredspawnRoutine => _deferredSpawnRoutine;
-
-        /// <summary>
-        /// 자식 클래스에서 OnSpawn() 대신 호출할 안전한 스폰 진입점
-        /// </summary>
-        public void SafeSpawn()
-        {
-            if (_deferredSpawnRoutine != null) return;
-            _deferredSpawnRoutine = DeferredSpawnRoutine();
-            StartCoroutine(_deferredSpawnRoutine);
-        }
-
-        private IEnumerator DeferredSpawnRoutine()
-        {
-            // 씬 시스템(ManagerHub 등)이 준비될 때까지 대기
-            while (!IsSceneReady) yield return null;
-
-            // 대기 도중 풀로 반환(비활성)되거나 파괴되었다면 즉시 탈출
-            if (this == null || !gameObject.activeInHierarchy)
-            {
-                _deferredSpawnRoutine = null;
-                yield break;
-            }
-
-            // 안전한 타이밍에 자식 클래스의 실제 로직 실행
-            OnSafeSpawn();
-            _deferredSpawnRoutine = null;
-        }
-
-        /// <summary>
-        /// 씬 인프라가 보장되고, 객체가 활성화된 상태임이 확실할 때 실행되는 스폰 로직
-        /// 자식 클래스는 이 메서드를 오버라이드하여 부품 조립 및 상태 시동을 진행
-        /// </summary>
-        protected virtual void OnSafeSpawn()
-        {
-        }
+        
     }
 }
