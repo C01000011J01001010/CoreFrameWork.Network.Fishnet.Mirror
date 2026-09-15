be4d1dc76cb8ef9ae535d73b266a0b0bcd068c49
YoonChangJoon <ycjun1209@gmail.com>
2026-09-15 18:21:19 +0900

    feature: BaseNetworkActorHost 추가 (내부적으로 FeatureHandler 사용)

---
+++ CoreEngine/Leaf/ActorHost/BaseNetworkActorHost.cs
@@ -0,0 +1,25 @@
+﻿using CoreEngine.Actor;
+using CoreEngine.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace CoreEngine.Network.FishNetExtension
+{
+    public abstract class BaseNetworkActorHost : BaseNetworkActor, IActorHost
+    {
+        protected readonly FeatureHandler FeatureHandler = new();
+
+        public override void Awake()
+        {
+            base.Awake();
+            FeatureHandler.SetHost(this);
+        }
+
+        public bool TryGetFeature<T>(out T feature) where T : class, IActorFeature
+        {
+            return FeatureHandler.TryGetFeature(out feature);
+        }
+    }
+}
